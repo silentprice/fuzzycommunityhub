99ae499a721fcf4ffa74eeddac525b9cf2a25f4d
nickytrickie nastykiD <nprice0502@gmail.com>
2025-06-30 21:27:44 -0400

    updated the footer, still alittle larger than I like, but seems to be issue with image size and border size. will try different versions

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,32 @@
-import xIcon from '../assets/xlogo.png'
-import dexIcon from '../assets/dexlogo.jpg'
-import cafeIcon from '../assets/cafelogo.png'
+import xIcon from '../assets/xlogo.png';
+import dexIcon from '../assets/dexlogo.jpg';
+import cafeIcon from '../assets/cafelogo.png';
+import fuzzyXblue from '../assets/FUZZYonXblue.png';
 import './footer.css';
 
-
 function Footer() {
-
-    return (
-        <footer>
-       
-                <div className="socialsLogos">
-                    <a href="https://x.com/fuzzy_xrp" target="_blank" rel="noopener noreferrer">
-                    <img className="socialsLogo" src={xIcon}></img>
-                    </a>
-                    <a href="https://dexscreener.com/xrpl/46555a5a59000000000000000000000000000000.rhcat4hrdi2y9pundkpmzxrdka5wkppr62_xrp" target="_blank" rel="noopener noreferrer">
-                    <img className="socialsLogo" src={dexIcon} alt="Dexscreener Link" />
-                    </a>
-                    <a href="https://xrp.cafe/collection/fuzzybears" target="_blank" rel="noopener noreferrer">
-                    <img className="socialsLogo" src={cafeIcon} alt="Cafe Link" />
-                    </a>
-                </div>
-            
-        </footer>
-
-    )
+  return (
+    <footer className="footer">
+      <div className="footerContent">
+        <img className="fuzzonxblue-logo" src={fuzzyXblue} alt="Fuzzy on X Blue Logo" />
+        <div className="socialsLogos">
+          <a href="https://x.com/fuzzy_xrp" target="_blank" rel="noopener noreferrer">
+            <img className="socialsLogo" src={xIcon} alt="X Social Link" />
+          </a>
+          <a
+            href="https://dexscreener.com/xrpl/46555a5a59000000000000000000000000000000.rhcat4hrdi2y9pundkpmzxrdka5wkppr62_xrp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="socialsLogo" src={dexIcon} alt="Dexscreener Link" />
+          </a>
+          <a href="https://xrp.cafe/collection/fuzzybears" target="_blank" rel="noopener noreferrer">
+            <img className="socialsLogo" src={cafeIcon} alt="XRP Cafe Link" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-
-export default Footer
+export default Footer;
